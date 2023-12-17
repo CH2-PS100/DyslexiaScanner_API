276c682321f999d4fafd6c9a2b105b35bf634975
Nerd <72287783+AdrianBhp43@users.noreply.github.com>
2023-12-18 00:21:43 +0700

    add .env and delete credentials.json

--- main.py
+++ main.py
@@ -3,14 +3,34 @@ import uvicorn
 import os
 import numpy as np
 import tensorflow as tf
+from dotenv import load_dotenv
 import firebase_admin
-from firebase_admin import firestore, credentials
+from firebase_admin import credentials, firestore, initialize_app
 from PIL import Image
 from io import BytesIO  # Add this line to import BytesIO
 
 app = FastAPI()
 
-cred = credentials.Certificate("./credentials.json")
+# Load environment variables from .env file
+load_dotenv()
+
+# Create a dictionary with Firebase credentials
+firebase_credentials = {
+    "type": os.getenv("FIREBASE_TYPE"),
+    "project_id": os.getenv("FIREBASE_PROJECT_ID"),
+    "private_key_id": os.getenv("FIREBASE_PRIVATE_KEY_ID"),
+    "private_key": os.getenv("FIREBASE_PRIVATE_KEY").replace("\\n", "\n"),
+    "client_email": os.getenv("FIREBASE_CLIENT_EMAIL"),
+    "client_id": os.getenv("FIREBASE_CLIENT_ID"),
+    "auth_uri": os.getenv("FIREBASE_AUTH_URI"),
+    "token_uri": os.getenv("FIREBASE_TOKEN_URI"),
+    "auth_provider_x509_cert_url": os.getenv("FIREBASE_AUTH_PROVIDER_X509_CERT_URL"),
+    "client_x509_cert_url": os.getenv("FIREBASE_CLIENT_X509_CERT_URL"),
+    "universe_domain": os.getenv("FIREBASE_UNIVERSE_DOMAIN")
+}
+
+# Use the credentials to initialize Firebase
+cred = credentials.Certificate(firebase_credentials)
 firebase_admin.initialize_app(cred)
 
 db = firestore.client()
